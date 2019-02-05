ff1d181db75b2cb04beeb0b0fd6e343082bf64f1
Junior Silva <n_junior83@hotmail.com>
2019-02-05 00:15:35 -0200

    Tela Filmes criada / Classe Modelo Films criada

--- StarTrivia/Controller/FilmsViewController.swift
+++ StarTrivia/Controller/FilmsViewController.swift
@@ -10,12 +10,44 @@ import UIKit
 
 class FilmsViewController: UIViewController, PersonProtocol {
     
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var lblEpisode: UILabel!
+    @IBOutlet weak var lblDirector: UILabel!
+    @IBOutlet weak var lblProducer: UILabel!
+    @IBOutlet weak var lblReleased: UILabel!
+    @IBOutlet weak var txtOpening: UITextView!
+    
+    @IBOutlet weak var btnPrevious: FadeEnabledButton!
+    @IBOutlet weak var btnNext: FadeEnabledButton!
+    
     var person: Person!
+    var films = [String]()
+    var currentFilm = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        print(person.name)
+        
+    }
+    
+    fileprivate func getFilm(url: String) {
+        
+    }
+    
+    @IBAction func previousClicked(_ sender: FadeEnabledButton) {
+        currentFilm -= 1
+        setButtonState()
+    }
+    
+    @IBAction func nextClicked(_ sender: FadeEnabledButton) {
+        currentFilm += 1
+        setButtonState()
+    }
+    
+    func setButtonState() {
+        btnPrevious.isEnabled = currentFilm == 0 ? false : true
+        btnNext.isEnabled = currentFilm == films.count - 1 ? false : true
+        getFilm(url: films[currentFilm])
     }
 
 }
---
+++ StarTrivia/Model/Film.swift
@@ -0,0 +1,29 @@
+//
+//  Film.swift
+//  StarTrivia
+//
+//  Created by Junior Silva on 05/02/19.
+//  Copyright © 2019 Junior Silva. All rights reserved.
+//
+
+import Foundation
+
+struct Film: Codable {
+    
+    let title: String
+    let episode: String
+    let opening: String
+    let director: String
+    let producer: String
+    let release: String
+    
+    enum CodingKeys: String, CodingKey {
+        case title
+        case episode = "episode_id"
+        case opening = "opening_crawl"
+        case director
+        case producer
+        case release = "release_date"
+    }
+    
+}
